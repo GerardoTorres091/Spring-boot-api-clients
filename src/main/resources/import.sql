dc01190f1ed8b4b2381f75e607f897858cf5fa79
Gerardo Torres Alonso <torresgerardo091@gmail.com>
2022-05-31 19:53:37 -0500

    commit initial

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gerardo','Torres','correo1@gmail.com','2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gerardo','Torres','correo2@gmail.com','2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gerardo','Torres','correo3@gmail.com','2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gerardo','Torres','correo4@gmail.com','2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gerardo','Torres','correo5@gmail.com','2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gerardo','Torres','correo6@gmail.com','2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gerardo','Torres','correo7@gmail.com','2018-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gerardo','Torres','correo8@gmail.com','2018-01-01');
